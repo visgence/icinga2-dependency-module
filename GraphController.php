f49f38736f003ed5f1ce17f851204fa57936417d
Jacob Bakarich <jacob.bakarich@gmail.com>
2017-09-30 11:50:20 -0600

    started working on docker container

---
+++ GraphController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Icinga\Module\dependencies\Controllers;
+
+use Icinga\Web\Controller;
+
+class GraphController extends Controller{
+
+        public function displayAction(){
+        }
+}
+?>
