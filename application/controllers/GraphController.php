188a03c3bf0c02359a74b9b6f4a03162cc2e8ebf
Jacob Bakarich <jacob.bakarich@gmail.com>
2017-10-02 11:34:10 -0600

    Docker startup script correctly implementing all config settings including dependency rules, Custom module now delivering  JSON from api to icinga web.
    -dependency _tool.py
    changed login information from root to a new admin user "icingaadmin"
    and "icinga"
    
    -zones.conf
    added director global zone to enable deployment of incinga director changes.
    
    -api-users.conf
    added new admin
    
    -parents.conf
    added parent dependency apply rule.
    
    -configuration.php
    added configuration file for module. adds menu icon and button to icinga 2 web.
    
    -display.phtml
    Moved to correct directory structure for icinga module support.
    
    Imported jquery, uses jquery to call getDependency function from controller, logs data, and prints first adress result.
    
    -GraphController.php
    moved to new folder for correct icinga module directory hierarchy.
    
    Uses php's curl function to make a get request to icinga 2 api, then echoes the response.

---
+++ application/controllers/GraphController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Icinga\Module\dependency_plugin\Controllers;
+
+use Icinga\Web\Controller;
+
+class GraphController extends Controller{
+
+        public function displayAction(){
+        }
+
+        public function getdependencyAction() {
+                    $this->_helper->layout->disableLayout();
+                    $response = $this->getResponse();
+                    $response->setHeader('Content-Type', 'application/json', true);
+                    $response->sendHeaders();
+                    
+
+
+                    $request_url = 'https://e35760e14397:5665/v1/objects/hosts';
+$username = 'root';
+$password = '911b092cf0530a34';
+$headers = array(
+    'Accept: application/json',
+    'X-HTTP-Method-Override: GET'
+);
+
+$ch = curl_init();
+
+curl_setopt_array($ch, array(
+
+    CURLOPT_URL => $request_url,
+    CURLOPT_HTTPHEADER => $headers,
+    CURLOPT_USERPWD => $username . ":" . $password,
+    CURLOPT_RETURNTRANSFER => true,
+    // CURLOPT_CAINFO => "/etc/apache2/ssl/icinga2.cert",
+    CURLOPT_SSL_VERIFYPEER => false,
+));
+
+$response = curl_exec($ch);
+
+if($response === false){
+    print "Error:" . curl_error($ch) . "(" . $response . ")\n";
+}
+
+$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+echo $response;
+        }
+}
+?>
---
+++ configuration.php
@@ -0,0 +1,6 @@
+<?php
+$section = $this->menuSection('Dependencies', array(
+    'url' => 'dependency_plugin/graph/display',
+    'icon' => 'thumbs-up'
+));
+?>
