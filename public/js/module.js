70dde9735ce20c0a41453f2148a8b3ad9d940c88
Jacob Bakarich <jacob.bakarich@gmail.com>
2017-10-18 11:49:58 -0600

    Fixed problem with ajax request being made too early, due to module.js being executed on page load. fix is WIP.
    -module.js
    Added timeout functions that listen for URL changes so ajax requests are only made when /graph (the dependency plugin) is the active window.

--- public/js/module.js
+++ public/js/module.js
@@ -1,28 +1,71 @@
 (function (Icinga) {
+
     var dependencies;
     var hosts;
     // Double ajax request to getHostsAction() and getDependencyAction() in module php controller file to retrieve JSONs for hosts, dependencies
-    $.when(
-        $.ajax({
-            url: "/icingaweb2/dependency_plugin/graph/getHosts",
-            type: 'GET',
-            success: function (hostData) {
-                hosts = (JSON.parse(hostData));
-            }
-        }),
 
-        $.ajax({
-            url: "/icingaweb2/dependency_plugin/graph/getDependency",
-            type: 'GET',
-            success: function (dependencyData) {
-                dependencies = (JSON.parse(dependencyData));
+    var pushUrl = function (href) {
+        history.pushState({}, '', href);
+        window.dispatchEvent(new Event('popstate'));
+        console.log('WAT');
+    };
+
+    timeout();
+
+    function timeout() {
+        setTimeout(function () {
+
+            if (window.location.href.endsWith('display')) {
+
+                $.when(
+                    $.ajax({
+                        url: "/icingaweb2/dependency_plugin/graph/getHosts",
+                        type: 'GET',
+                        success: function (hostData) {
+                            hosts = (JSON.parse(hostData));
+                        }
+                    }),
+
+                    $.ajax({
+                        url: "/icingaweb2/dependency_plugin/graph/getDependency",
+                        type: 'GET',
+                        success: function (dependencyData) {
+                            dependencies = (JSON.parse(dependencyData));
+                        }
+                    })
+                ).then(function () {
+                    console.log(hosts);
+                    console.log(dependencies);
+                    drawDependencies(hosts, dependencies);
+                });
+
+
+            exitTimeout();
+
+            } else {
+
+                timeout();
             }
-        })
-    ).then(function () {
-        console.log(hosts);
-        console.log(dependencies);
-        drawDependencies(hosts, dependencies);
-    });
+        }, 1000);
+
+
+
+    }
+
+    function exitTimeout(){
+        setTimeout(function () {
+
+            if (!window.location.href.endsWith('display')) {
+             timeout();   
+            }else{
+                exitTimeout();
+            }
+        }, 1000);
+
+    }
+
+
+
+
 
 }(Icinga));
-
