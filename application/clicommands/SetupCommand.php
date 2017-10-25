212580bb336bacb2a27648c39480b8c87537b586
Jacob Bakarich <jacob.bakarich@gmail.com>
2017-10-25 10:27:14 -0600

    Created SetupCommand.php, which enables a icingacli command "setup api" which prompts user for input on icinga 2 web api login information, and then writes it to a config.ini file in /etc/icingaweb2/modules/dependency_module

---
+++ application/clicommands/SetupCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Icinga\Module\dependency_plugin\Clicommands;
+
+use Icinga\Cli\Command;
+
+class SetupCommand extends Command{
+
+    public function apiAction()
+    {
+        echo "Please Enter API Username:";
+        $handle = fopen('php://stdin', 'r');
+        $user = fgets($handle);
+
+        echo "Please Enter API Password:";
+        system('stty -echo');
+        $password = fgets(STDIN);
+        system('stty echo');
+        echo("\n");
+
+        echo $user, $password;
+
+        $user = str_replace("\n", '', $user);
+        $password = str_replace("\n", '', $password);
+
+        $apiLogin = (object)['user' => $user, 'password'=>$password];
+    
+        
+        $apiLogin = json_encode($apiLogin);
+
+        mkdir('/etc/icingaweb2/modules/dependency_plugin');
+
+        $file = fopen('/etc/icingaweb2/modules/dependency_plugin/config.ini', 'w');
+        
+        fwrite($file, $apiLogin);
+
+        fclose($file);
+
+
+    }
+}
